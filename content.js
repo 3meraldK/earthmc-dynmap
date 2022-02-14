d0290cad4ad560366b406ebcd437b8e812dd37a9
3meraldK <ooyaxcontact@gmail.com>
2022-02-14 18:47:47 +0100

    remove useless divs

---
+++ content.js
@@ -0,0 +1,8 @@
+const interval = setInterval(() => {
+	if (document.getElementsByClassName('playerNameSm playerNameNoHealth').length > 0) {
+		document.getElementsByClassName('compass compass_S compass_flat')[0].remove();
+		document.getElementsByClassName('largeclock timeofday')[0].remove();
+		document.getElementsByClassName('leaflet-bottom leaflet-right')[0].remove();
+		clearInterval(interval);
+	} 
+}, 1000);
