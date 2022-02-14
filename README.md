4623ba9ddbee1ea7e4e724b783580400217c7b20
3meraldK <ooyaxcontact@gmail.com>
2022-02-14 17:45:38 +0100

    code revamp

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
 # earthmc-dynmapcolor
-Colors meganations on the map and more. This project is a remake of https://github.com/FranchuFranchu/emc-dynmap-colors, but removes all star markers and solidifies the towns' strokes. Right now it's supported only on Mozilla Firefox.
+This Mozilla Firefox add-on enriches the EarthMC dynmap with a meganation support, i.e. now colors entire meganation the same color, and more.
 
 # Download and use
+This plugin is supported only on Mozilla Firefox due to Chromium limitations, however you may use [this userscript](https://github.com/32Vache/emc-map-colors) on Chromium browsers (MS Edge, Chrome).
+
 1. Download the latest 'extension.zip' asset from https://github.com/3meraldK/earthmc-dynmapcolor/releases.
 2. In the normal browser, search "about:debugging#/runtime/this-firefox" and then load an add-on via the button, by selecting the file. The add-on will be usable on a one browser session meaning that you have to repeat this step every time you reopen the browser. This is an unsigned plugin.
 3. However if you want to permanently install the extension, you may use the Firefox Developer Edition browser.
     1. Search "about:config", accept the prompt and search for 'xpinstall.signatures.required' and toggle the value to false.
     2. Restart the browser.
     3. Search "about:addons" and import the add-on from the file.
-     
-# TO-DO
-1. Support for Chromium browsers
-2. Beautification of the code
-3. Fix the disappearance of players
-4. Add meganation label when town information is displayed (if possible)
--- background.js
+++ background.js
@@ -1,129 +1,94 @@
-let NATION_COLORS = {}
-let NATION_COLORS_FLAT = []
+const dataURL = 'https://raw.githubusercontent.com/3meraldK/earthmc-dynmapcolor/main/data.json';
+const markerURL = 'https://earthmc.net/map/tiles/_markers_/marker_earth.json';
 
-function recalculateNationColors() {
-  NATION_COLORS = {}
-  for (let meganation of NATION_COLORS_FLAT) {
-    
-    for (let nation of meganation.nations) {
-      NATION_COLORS[nation.toLowerCase()] = meganation.color;
-    }
-  }
+// Get meganations when extension starts.
+var meganations;
+fetch(dataURL)
+	.then(response => response.json())
+	.then(data => meganations = data);
+setInterval(() => {
+	fetch(dataURL)
+		.then(response => response.json())
+		.then(data => meganations = data);
+}, 1000*60*5);
+
+// Listens for request sending.
+browser.webRequest.onBeforeRequest.addListener(
+	onMapUpdate, 
+	{urls: [markerURL]}, 
+	['blocking']
+);
+
+// Function is fired when the request is sent.
+function onMapUpdate(details) {
+
+	// Decoding/encoding utilizations, and StreamFilter object.
+	let decoder = new TextDecoder('utf-8');
+	let encoder = new TextEncoder();
+	var filter = browser.webRequest.filterResponseData(details.requestId);
+
+	// Get the response.
+	let arrayBuffer = [];
+	filter.ondata = event => arrayBuffer.push(decoder.decode(event.data, {stream: true}));
+
+	// Fired when response is sent.
+	filter.onstop = () => {
+
+		// Decode the response.
+		arrayBuffer.push(decoder.decode());
+		let string = arrayBuffer.join('');
+		let data = JSON.parse(string);
+
+		// Check if response is undefined.
+		if (data.sets === undefined) return;
+
+		// Delete star icons.
+		delete data.sets["townyPlugin.markerset"].markers;
+
+		// Iterate through every town.
+		Object.values(data.sets["townyPlugin.markerset"].areas).forEach(town => {
+
+			// Settings for every town.
+			town.weight = 1.6;
+			town.opacity = 1;
+
+			// Check if a town contains any (<text>) in their description.
+			let townTitle = town.desc.match(/\([^ ]+\)/g);
+			if (townTitle === null || townTitle === undefined || townTitle.length < 1) return;
+
+			// Remove (Shop) from the description if existent.
+			if (townTitle.indexOf('(Shop)') > -1) townTitle.splice(townTitle.indexOf('(Shop)'), 1);
+
+			// Set every town's color to default.
+			town.color = '#3FB4FF';
+			town.fillcolor = '#3FB4FF';
+			if (townTitle === null || townTitle === undefined || townTitle.length < 1) {
+				town.color = '#89C500';
+				town.fillcolor = '#89C500';
+				return;
+			};
+
+			// Get rid of that array and brackets.
+			let nation = townTitle[0];
+			nation = nation.replace(/[()]/g, '');
+
+			// Check if town's nation is in any meganation, if yes then apply colors and add a description.
+			meganations.forEach(meganation => {
+
+				if (!meganation.nations.includes(nation)) return;
+				town.color = meganation.color;
+				town.fillcolor = meganation.color;
+				town.desc = town.desc.replace(')</span><br />', ')</span><br /> ' + 
+					'<span style=\"font-size:80%\">Part of</span> ' + 
+					'<span style=\"font-size:90%\"><b>' + meganation.name + '</b></span><br />');
+			});
+
+		});
+
+		// Send the modified response
+		filter.write(encoder.encode(JSON.stringify(data)));
+
+		// Close the filter
+		filter.close();
+	};
 }
-
-console.log(browser.runtime.getURL("data.json"));
-fetch(new Request(browser.runtime.getURL("data.json")))
-  .then(function(response) {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.text();
-  })
-  .then(function(text) {
-    NATION_COLORS_FLAT = JSON.parse(text)
-    recalculateNationColors()
-    
-    browser.webRequest.onBeforeRequest.addListener(
-      requestListener,
-      {urls: ["*://earthmc.net/map/tiles/_markers_/marker_earth.json", "*://earthmc.net/map/up/world/earth/*"]},
-      ["blocking"]
-    );
-    console.log("All set up!")
-  })
-
-
-fetch("/data.json")
-  .then(function(response) {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.text();
-  })
-  .then(function(text) {
-    NATION_COLORS_FLAT = JSON.parse(text)
-    recalculateNationColors()
-    
-    console.log("Fetched updated colors!")
-  })
-  .catch(error => console.warn("Error while loading recent data:", error.message));
-  
- 
-function shouldPaint(town) {
-  let desc_title = town.desc.match(/<span style=\"font-size:120%\">(.+?)<\/span>/)
-  if (!desc_title) {
-    return;
-  }
-  desc_title = desc_title[1];
-  if (!desc_title) {
-    return;
-  }
-  let nation = desc_title.match(/.+? \((.+?)\)$/)
-  if (!nation) {
-    return;
-  }
-  nation = nation[1];
-  if (!nation) {
-    return;
-  }
-  if (NATION_COLORS[nation.toLowerCase()]) {
-    return NATION_COLORS[nation.toLowerCase()]
-  } else {
-    return ["#3FB4FF"];
-  }
-}
-
-
-  function requestListener(details) {
-    var filter = browser.webRequest.filterResponseData(
-      details.requestId
-    )
-    let decoder = new TextDecoder("utf-8");
-    let encoder = new TextEncoder();
-     console.log("Called!");
-    let datar = [];  
-    filter.ondata = event => {
-      console.log("Called f!");
-      datar.push(event.data);
-    }
-
-    filter.onstop = event => {
-      console.log("ended");
-      let str = "";
-      if (datar.length == 1) {
-        str = decoder.decode(datar[0]);
-      }
-      else {
-        for (let i = 0; i < datar.length; i++) {
-          let stream = (i == datar.length - 1) ? false : true;
-          str += decoder.decode(datar[i], {stream});
-        }
-      }
-      let data = JSON.parse(str);
-      
-      if (data.sets === undefined) {
-         console.log("Non-map update")
-         return;
-      } else {
-        console.log("Map update")
-      }
-
-      delete data.sets["townyPlugin.markerset"].markers;
-      
-      Object.values(data.sets["townyPlugin.markerset"].areas).forEach(town => {
-
-        let paint = shouldPaint(town)
-        //let paint = shouldPaint(town)
-        town.weight = 1.6;
-        town.opacity = 1;
-        if (paint) {
-          town.fillcolor = paint[0];
-          // DEfault to fillcolor if color undefined
-          town.color = paint[1] || paint[0];
-        }
-      })
-      
-      filter.write(encoder.encode(JSON.stringify(data)));
-      
-      filter.close();
-    }
-  }
