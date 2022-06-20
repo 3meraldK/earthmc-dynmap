efca6cfcc35a320182867265fa066ba95ab9fa8a
3meraldK <ooyaxcontact@gmail.com>
2022-06-20 12:18:18 +0200

    1.7 release

--- README.md
+++ README.md
@@ -1,22 +1,19 @@
 ![Release](https://img.shields.io/github/v/release/3meraldK/earthmc-dynmapcolor) ![Repo size](https://img.shields.io/github/repo-size/3meraldK/earthmc-dynmapcolor) ![Downloads](https://img.shields.io/github/downloads/3meraldK/earthmc-dynmapcolor/total)
 
-# earthmc-dynmapcolor
-This Mozilla Firefox add-on enriches the EarthMC dynmap, i.e. colors entire meganations/alliances the same color, and more. 
+# earthmc-dynmap+
+This Firefox extension enriches the EarthMC dynmap with various features, such as coloring meganations and alliances the same color, beautifying elements of the site and more.
 
 ## Updates
-Meganation data should be maintained regularly. If you govern the meganation or alliance, and want to modify it, please contact the owner through Discord by the name 3meraldK#2137.
+Alliances data is based on EMC Toolkit database, which their team accepts requests on their [discord](https://discord.gg/AVtgkcRgFs).
 
-# Download and use
-This plugin is supported only on Mozilla Firefox due to Chromium limitations, however you may use [this userscript](https://github.com/32Vache/emc-map-colors) on Chromium browsers (MS Edge, Chrome, Opera, Brave). Installation course:
+## Download and use
+This plugin is supported only on Firefox due to Chromium limitations, however you may use [this userscript](https://github.com/32Vache/emc-map-colors) on Chromium browsers (MS Edge, Chrome, Opera, Brave). Installation course for Mozilla:
 
 1. Download the latest 'extension.zip' asset from [the release page](https://github.com/3meraldK/earthmc-dynmapcolor/releases).
-2. In the normal browser, search "about:debugging#/runtime/this-firefox" and then load an add-on via the button, by selecting the file. The add-on will be usable on a one browser session meaning that you have to repeat this step every time you reopen the browser. That is because it will likely be unsigned plugin.
-3. However if you want to permanently install the extension, you may use the Firefox Developer Edition browser.
-    1. Search "about:config", accept the prompt and search for 'xpinstall.signatures.required' and toggle the value to false.
-    2. Restart the browser.
-    3. Search "about:addons" and import the add-on from the file.
-    4. Restart the browser and ensure the add-on is installed, by searching "about:addons" again (because it may bug).
+2. Now, you can choose two browsers; Firefox allows for one-time use per session, i.e. it will be removed after you close the browser - that is because the plugin is unsigned. [Mozilla Developer Edition](https://www.mozilla.org/en-US/firefox/developer) however will pernamently keep its add-ons on the list.
+3. In the normal edition, search "about:debugging#/runtime/this-firefox" and then load an add-on via the button, by selecting the file.
+4. In the developer edition, search "about:config", then search "xpinstall.signatures.required" and set it to false. Restart the browser, search "about:addons" and import the add-on from the asset. Make sure it has been installed because sometimes it might not.
 
-# Screenshots
-![image](https://user-images.githubusercontent.com/48335651/153918902-67f8741d-323e-422e-ae39-fda503359b21.png)
-![image](https://user-images.githubusercontent.com/48335651/153919033-70b78831-7d3a-4172-87d8-a5b774968c50.png)
+## Screenshots
+![image](https://user-images.githubusercontent.com/48335651/174450062-ff2e7396-181e-4cd2-9fd6-42fe148c5767.png)
+![image](https://user-images.githubusercontent.com/48335651/174450070-42b0f815-05fd-4999-9d46-47c9adac3901.png)
--- background.js
+++ background.js
@@ -1,13 +1,24 @@
 const encoder = new TextEncoder();
 const decoder = new TextDecoder('utf-8');
-const novaURL = 'https://raw.githubusercontent.com/3meraldK/earthmc-dynmapcolor/main/data-nova.json';
-const auroraURL = 'https://raw.githubusercontent.com/3meraldK/earthmc-dynmapcolor/main/data-aurora.json';
-const fetchAlliances = (map) => fetch(map).then(res => res.json()).catch(() => {});
-const buttonEvent = () => whatIsFetched == 'meganation' ? whatIsFetched = 'alliance' : whatIsFetched = 'meganation';
-var whatIsFetched = 'meganation';
+const novaURL = 'https://earthmc-api.herokuapp.com/api/v1/nova/alliances';
+const auroraURL = 'https://earthmc-api.herokuapp.com/api/v1/aurora/alliances';
+const fetchAlliances = (url) => fetch(url).then(res => res.json()).catch(() => {});
+const buttonEvent = () => mapMode == 'mega' ? mapMode = 'normal' : mapMode = 'mega';
+var mapMode = 'mega';
 browser.runtime.onMessage.addListener(buttonEvent);
 
-// Listens for requests.
+// Courtesy of 32Vache :)
+function calcArea(x, y, ptsNum) {
+	var area = 0;
+	var j = ptsNum - 1;
+	for (var i = 0; i < ptsNum; i++) {
+		area = area + (x[j] + x[i]) * (y[j] - y[i]);
+		j = i;
+	}
+	return (Math.abs(area / 2)) / 256;
+}
+
+// Listening for browser requests.
 browser.webRequest.onBeforeRequest.addListener(
 	function listener(details) { details.url.includes('up/world/earth/') ? onPlayerUpdate(details) : onMapUpdate(details); }, 
 	{urls: ['https://earthmc.net/map/nova/tiles/_markers_/marker_earth.json', 
@@ -17,81 +28,117 @@ browser.webRequest.onBeforeRequest.addListener(
 	['blocking']
 );
 
-// Function is fired when the marker_earth request is sent.
+// Managing the map update response.
 function onMapUpdate(details) {
-	var usedMap;
-	details.url.includes('nova') ? usedMap = 'nova' : usedMap = 'aurora';
-	const filter = browser.webRequest.filterResponseData(details.requestId);
 
-	// Get the response.
+	// Getting the response.
+	const filter = browser.webRequest.filterResponseData(details.requestId);
 	const arrayBuffer = [];
 	filter.ondata = event => arrayBuffer.push(decoder.decode(event.data, {stream: true}));
-	// Fired when response is sent.
+
+	// Modifying the response.
 	filter.onstop = () => {
-		// Decode the response.
+
 		arrayBuffer.push(decoder.decode());
 		const data = JSON.parse(arrayBuffer.join(''));
-
-		// Check if response is undefined and delete star icons.
-		if (data.sets === undefined) return;
+		if (data.sets == undefined) return;
+		
+		// Delete star icons.
 		delete data.sets["townyPlugin.markerset"].markers;
 
-		fetchAlliances(usedMap == 'nova' ? novaURL : auroraURL).then(meganations => {
+		// Iterating through every area drawn on the map.
+		Object.values(data.sets["townyPlugin.markerset"].areas).forEach(town => {
 
-			// Iterate through every town.
+			// Some variables.
+			var townTitle = town.desc.split('<br \/>')[0];
+			townTitle = townTitle.replace(/\(Shop\)$/g, '').replaceAll(/[()]/g, '').split(' ');
+			const nation = townTitle[2].replace('</span>', '');
+			const area = calcArea(town.x, town.z, town.x.length);
+			const memberList = town.desc.split('Members <span style=\"font-weight:bold\">')[1].split('</span><br />Flags')[0];
+			const memberSize = (memberList.match(/,/g) || []).length + 1;
+
+			// Removing shop areas.
+			if (town.desc.includes('(Shop) (')) {
+				town.fillopacity = 0;
+				town.opacity = 0;
+			}
+
+			// Recreating town's description.
+			town.desc = town.desc.replace('>hasUpkeep:', '>Has upkeep:').replace('>pvp:', '>PVP allowed:').replace('>mobs:', '>Mob spawning:').replace('>public:', '>Public status:').replace('>explosion:', '>Explosions:').replace('>fire:', '>Fire spread:').replace('>capital:', '>Is capital:');
+			town.desc = town.desc.replaceAll('true<', '\u2705<').replaceAll('false<', '\u26D4<');
+			town.desc = town.desc.replace('Members <span', 'Members <b>[' + memberSize + ']</b> <span');
+			town.desc = town.desc.replace('</span><br /> Members', '</span><br />Size<span style=\"font-weight:bold\"> ' + area + ' </span><br /> Members');
+
+			// Modifying esthetics of the area.
+			town.weight = 1;
+			town.opacity = 0.7;
+			if (town.color == "#3FB4FF" && town.fillcolor == "#3FB4FF") town.color = "#000000", town.fillcolor = "#000000";
+			if (nation.length < 1) {
+				town.color = '#83003F';
+				town.fillcolor = '#83003F';
+				return;
+			}
+		})
+
+		// Fetching alliances from appriopriate worlds.
+		details.url.includes('nova') ? usedMap = 'nova' : usedMap = 'aurora';
+		fetchAlliances(usedMap == 'nova' ? novaURL : auroraURL).then(alliances => {
+
 			Object.values(data.sets["townyPlugin.markerset"].areas).forEach(town => {
 
-				// Settings for every town.
-				town.weight = 1.6;
-				town.opacity = 1;
-
-				townTitle = town.desc.split('<br \/>')[0];
+				var townTitle = town.desc.split('<br \/>')[0];
 				townTitle = townTitle.replace(/\(Shop\)$/g, '').replaceAll(/[()]/g, '').split(' ');
-				const nation = townTitle[2].toLowerCase().replace('</span>', '');
-
-				// Set every town's color to default.
-				town.color = '#3FB4FF';
-				town.fillcolor = '#3FB4FF';
-				if (nation.length < 1) {
-					town.color = '#89C500';
-					town.fillcolor = '#89C500';
-					return;
-				};
-
-				// Get rid of an array and brackets.
+				const nation = townTitle[2].replace('</span>', '');
 				var meganationList = '';
 
-				// Check if town's nation is in any meganation.
+				alliances.forEach(alliance => {
 
-				meganations.forEach(meganation => {
-					if (meganation.type != whatIsFetched) return;
-					// Nation controlled by multiple meganations support.
-					if (!meganation.nations.includes(nation)) return;
-					meganationList.length < 1 ? meganationList += meganation.name : meganationList += ', ' + meganation.name;
-
-					// If yes, apply fill color (and stroke color if defined).
-					const fillColor = meganation.color[0];
-					var strokeColor = fillColor;
-					if (meganation.color.length == 2) strokeColor = meganation.color[1];
-					town.color = strokeColor;
-					town.fillcolor = fillColor;
-				});
+					// Some variables and their default values.
+					var allianceType, allianceName, allianceStrokeColor, allianceFillColor;
+					allianceType = alliance.type ?? 'mega';
+					allianceName = alliance.fullName ?? alliance.allianceName;
+					if (alliance.hasOwnProperty('colours')) {
+						allianceFillColor = alliance.colours.fill;
+						allianceStrokeColor = alliance.colours.outline;
+					} else {
+						allianceFillColor = "#000000";
+						allianceStrokeColor = "#000000";
+					}
 					
-				// Apply description.
+					if (allianceType != mapMode) return;
+					if (!alliance.nations.includes(nation)) return;
+					meganationList.length < 1 ? meganationList += allianceName : meganationList += ', ' + allianceName;
+
+					// Esthetics of alliance areas; their visuals differ from single nations and nationless towns.
+					if (!town.desc.includes('(Shop) (')) {
+						town.weight = 2.2;
+						town.opacity = 1;
+						town.color = allianceStrokeColor;
+						town.fillcolor = allianceFillColor;
+					}
+				});
+
+				// Assigning the alliance name to the town.
 				if (meganationList.length > 0) town.desc = town.desc.replace(')</span><br />', ')</span><br /> ' + 
 					'<span style=\"font-size:80%\">Part of</span> ' + 
 					'<span style=\"font-size:90%\"><b>' + meganationList + '</b></span><br />');
+				
 			});
 
-			// Send the modified response and close the filter.
+			// Sending the modified response.
 			filter.write(encoder.encode(JSON.stringify(data)));
 			filter.close();
-		})
-	};
+		}).catch((error) => {
+			console.log('Could not connect to the database! Error: ' + error);
+			filter.write(encoder.encode(JSON.stringify(data)));
+			filter.close();
+		});
+	}
 }
 
-// Fired each ~1 second when the update request is sent.
+// Managing the players' status response.
 function onPlayerUpdate(details) {
+
 	const filter = browser.webRequest.filterResponseData(details.requestId);
 	const arrayBuffer = [];
 	filter.ondata = event => arrayBuffer.push(decoder.decode(event.data, {stream: true}));
@@ -100,9 +147,9 @@ function onPlayerUpdate(details) {
 		arrayBuffer.push(decoder.decode());
 		const string = arrayBuffer.join('');
 		const data = JSON.parse(string);
-		// If response's length > 64 kB then trigger onMapUpdate(), otherwise write data to the filter
 		if (!data.currentcount) return;
+		// If response's size > 64kB, it's a map update.
 		string.length < 65536 ? filter.write(encoder.encode(JSON.stringify(data))) : onMapUpdate(details);
 		filter.close();
-	};
+	}
 }
