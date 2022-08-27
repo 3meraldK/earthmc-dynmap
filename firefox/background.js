1980e787d9622d88adbe0a49a2d56304afe315dd
3meraldK <ooyaxcontact@gmail.com>
2022-08-27 20:48:56 +0200

    Firefox 1.9

--- firefox/background.js
+++ firefox/background.js
@@ -2,12 +2,22 @@ const encoder = new TextEncoder();
 const decoder = new TextDecoder('utf-8');
 const novaURL = 'https://emc-toolkit.vercel.app/api/nova/alliances';
 const auroraURL = 'https://emc-toolkit.vercel.app/api/aurora/alliances';
-const fetchAlliances = (url) => fetch(url).then(res => res.json()).catch(() => {});
-const buttonEvent = () => mapMode == 'mega' ? mapMode = 'normal' : mapMode = 'mega';
-let mapMode = 'mega';
-browser.runtime.onMessage.addListener(buttonEvent);
+const fetchAlliances = (url) => fetch(url).then(res => res.json()).catch((error) => { console.error(`Could not fetch alliances: ${error}`); });
 
-// Courtesy of 32Vache :)
+// Webpage listener.
+let mapMode = 'mega';
+let date = 0;
+browser.runtime.onMessage.addListener(webpageListener);
+function webpageListener(message) {
+	if (message.message) {
+		mapMode == 'mega' ? mapMode = 'normal' : mapMode = 'mega';
+		date = 0;
+		return;
+	}
+	date = message.date.toLocaleDateString('sv').replaceAll('-', '');
+}
+
+// Courtesy of 32Vache.
 function calcArea(x, y, ptsNum) {
 	let area = 0;
 	let j = ptsNum - 1;
@@ -18,140 +28,163 @@ function calcArea(x, y, ptsNum) {
 	return (Math.abs(area / 2)) / 256;
 }
 
-// Listening for browser requests.
+// Listen for requests.
 browser.webRequest.onBeforeRequest.addListener(
-	function listener(details) { details.url.includes('up/world/earth/') ? onPlayerUpdate(details) : onMapUpdate(details); }, 
-	{urls: ['https://earthmc.net/map/nova/tiles/_markers_/marker_earth.json', 
-			'https://earthmc.net/map/nova/up/world/earth/*', 
-			'https://earthmc.net/map/aurora/tiles/_markers_/marker_earth.json', 
-			'https://earthmc.net/map/aurora/up/world/earth/*']}, 
-	['blocking']
+	function requestListener(details) { details.url.includes('up/world/earth/') ? onPlayerUpdate(details) : onMapUpdate(details); },
+	{ urls: ['https://earthmc.net/map/nova/tiles/_markers_/marker_earth.json',
+		'https://earthmc.net/map/nova/up/world/earth/*',
+		'https://earthmc.net/map/aurora/tiles/_markers_/marker_earth.json',
+		'https://earthmc.net/map/aurora/up/world/earth/*'] },
+	['blocking'],
 );
 
-// Managing the map update response.
+// Manage the map response.
 function onMapUpdate(details) {
 
-	// Getting the response.
 	const filter = browser.webRequest.filterResponseData(details.requestId);
 	const arrayBuffer = [];
-	filter.ondata = event => arrayBuffer.push(decoder.decode(event.data, {stream: true}));
 
-	// Modifying the response.
+	// Get the response.
+	filter.ondata = event => arrayBuffer.push(decoder.decode(event.data, { stream: true }));
+
+	// Modify the response.
 	filter.onstop = () => {
 
+		// Check the mode.
 		arrayBuffer.push(decoder.decode());
-		const data = JSON.parse(arrayBuffer.join(''));
-		if (!data.sets) return;
-		
-		// Modify star markers.
-		const markers = data.sets["townyPlugin.markerset"].markers;
-		Object.values(markers).forEach(marker => delete marker.desc);
-		delete data.sets["townyPlugin.markerset"].markers;
-		data.sets.markers.markers = markers;
+		const streamData = JSON.parse(arrayBuffer.join(''));
+		const server = (details.url.includes('nova')) ? 'nova' : 'aurora';
+		const urlProper = [`https://web.archive.org/web/${date}id_/https://earthmc.net/map`, 'tiles/_markers_/marker_earth.json'];
+		date < '20220428' ? url = `${urlProper[0]}/${urlProper[1]}` : url = `${urlProper[0]}/${server}/${urlProper[1]}`;
 
-		// Delete shop areas.
-		Object.keys(data.sets["townyPlugin.markerset"].areas).forEach(town => {
-			if (data.sets["townyPlugin.markerset"].areas[town].desc.includes('(Shop) (')) delete data.sets["townyPlugin.markerset"].areas[town];
-		});
-
-		// Iterating through every area drawn on the map.
-		Object.values(data.sets["townyPlugin.markerset"].areas).forEach(town => {
-
-			// Some variables.
-			let townTitle = town.desc.split('<br \/>')[0].replaceAll(/[()]/g, '').split(' ');
-			const nation = townTitle[2].replace('</span>', ''),
-				  area = calcArea(town.x, town.z, town.x.length),
-				  memberList = town.desc.split('Members <span style=\"font-weight:bold\">')[1].split('</span><br />Flags')[0],
-				  memberSize = (memberList.match(/,/g) || []).length + 1;
-
-			// Recreating town's description.
-			town.desc = town.desc.replace('\">hasUpkeep:', '; white-space:pre\">hasUpkeep:');
-			town.desc = town.desc.replace('>hasUpkeep: true<br />', '>')
-								 .replace('>pvp:', '>PVP allowed:')
-								 .replace('>mobs:', '>Mob spawning:')
-								 .replace('>public:', '>Public status:')
-								 .replace('>explosion:', '>Explosions:&#9;')
-								 .replace('>fire:', '>Fire spread:')
-								 .replace('<br />capital: false</span>', '</span>')
-								 .replace('<br />capital: true</span>', '</span>')
-		  						 .replaceAll('true<', '&#9;<span style="color:green">Yes</span><')
-			                     .replaceAll('false<', '&#9;<span style="color:red">No</span><')
-								 .replace('Members <span', 'Members <b>[' + memberSize + ']</b> <span')
-							 	 .replace('</span><br /> Members', '</span><br />Size<span style=\"font-weight:bold\"> ' + area + ' </span><br /> Members');
-
-			// Modifying esthetics of the area.
-			town.weight = 1;
-			town.opacity = 0.7;
-			if (town.color == "#3FB4FF" && town.fillcolor == "#3FB4FF") town.color = town.fillcolor = "#000000";
-			if (nation.length < 1) {
-				town.fillcolor = town.color = '#83003F';
-				return;
-			}
-		});
-
-		// Fetching alliances from appriopriate worlds.
-		fetchAlliances(details.url.includes('nova') ? novaURL : auroraURL).then(alliances => {
-
-			Object.values(data.sets["townyPlugin.markerset"].areas).forEach(town => {
-
-				let townTitle = town.desc.split('<br \/>')[0],
-				    meganationList = '';
-				townTitle = townTitle.replaceAll(/[()]/g, '').split(' ');
-				const nation = townTitle[2].replace('</span>', '');
-				
-				alliances.forEach(alliance => {
-
-					// Some variables and their default values.
-					let allianceType, allianceName, allianceStrokeColor, allianceFillColor;
-					allianceType = alliance.type ?? 'mega';
-					allianceName = alliance.fullName ?? alliance.allianceName;
-					if (alliance.hasOwnProperty('colours')) {
-						allianceFillColor = alliance.colours.fill;
-						allianceStrokeColor = alliance.colours.outline;
-					} else allianceFillColor = allianceStrokeColor = "#000000";
-					
-					if (allianceType != mapMode || !alliance.nations.includes(nation)) return;
-					meganationList.length < 1 ? meganationList += allianceName : meganationList += ', ' + allianceName;
-
-					// Esthetics of alliance areas; their visuals differ from single nations and nationless towns.
-					town.weight = 2.2;
-					town.opacity = 1;
-					town.color = allianceStrokeColor;
-					town.fillcolor = allianceFillColor;
+		if (date.length != 8) { execute(streamData, true, 'townyPlugin.markerset'); }
+		else {
+			const jsonPath = date < '20200322' ? 'towny.markerset' : 'townyPlugin.markerset';
+			fetch(url)
+				.then(res => res.json()).then(fetchData => execute(fetchData, false, jsonPath))
+				.catch((error) => {
+					console.error(`Could not fetch archives, exiting archive mode: ${error}`);
+					execute(streamData, true, 'townyPlugin.markerset');
 				});
+		}
 
-				// Assigning the alliance name to the town.
-				if (meganationList.length > 0) town.desc = town.desc.replace(
-					')</span><br />', ')</span><br /> ' + 
-					'<span style=\"font-size:80%\">Part of</span> ' + 
-					'<span style=\"font-size:90%\"><b>' + meganationList + '</b></span><br />');
+		// The main function.
+		function execute(data, drawAlliances, jsonPath) {
+
+			// Move markers so they can be toggled.
+			const markers = data.sets[jsonPath].markers;
+			Object.values(markers).forEach(marker => delete marker.desc);
+			delete data.sets[jsonPath].markers;
+			data.sets.markers.markers = markers;
+
+			// Delete shop areas.
+			Object.keys(data.sets[jsonPath].areas).forEach(town => {
+				if (data.sets[jsonPath].areas[town].desc.includes('(Shop) (')) delete data.sets[jsonPath].areas[town];
 			});
 
-			// Sending the modified response.
-			filter.write(encoder.encode(JSON.stringify(data)));
-			filter.close();
-		}).catch((error) => {
-			console.log('Could not connect to the database! Error: ' + error);
-			filter.write(encoder.encode(JSON.stringify(data)));
-			filter.close();
-		});
-	}
+			// Iterate through towns.
+			Object.values(data.sets[jsonPath].areas).forEach(town => {
+
+				const townTitle = town.desc.split('<br />')[0].replaceAll(/[()]/g, '').split(' '),
+					nation = townTitle[2].replace('</span>', ''),
+					area = calcArea(town.x, town.z, town.x.length),
+					members = (date && date < '20200410') ? 'Associates' : 'Members';
+				memberList = town.desc.split(`${members} <span style="font-weight:bold">`)[1].split('</span><br />Flags')[0],
+				memberSize = (memberList.match(/,/g) || []).length + 1;
+
+				// Paint alliances if allowed.
+				if (drawAlliances) {
+					if (town.color == '#3FB4FF' && town.fillcolor == '#3FB4FF') town.color = town.fillcolor = '#000000';
+					if (nation.length < 1) {
+						town.fillcolor = town.color = '#FF00FF';
+						return;
+					}
+				}
+
+				// Recreate town description and outline.
+				town.desc = town.desc.replace('">hasUpkeep:', '; white-space:pre">hasUpkeep:');
+				town.desc = town.desc.replace('>hasUpkeep: true<br />', '>')
+					.replace('>pvp:', '>PVP allowed:')
+					.replace('>mobs:', '>Mob spawning:')
+					.replace('>public:', '>Public status:')
+					.replace('>explosion:', '>Explosions:&#9;')
+					.replace('>fire:', '>Fire spread:')
+					.replace('<br />capital: false</span>', '</span>')
+					.replace('<br />capital: true</span>', '</span>')
+					.replaceAll('true<', '&#9;<span style="color:green">Yes</span><')
+					.replaceAll('false<', '&#9;<span style="color:red">No</span><')
+					.replace(`${members} <span`, `${members} <b>[${memberSize}]</b> <span`)
+					.replace(`</span><br /> ${members}`, `</span><br />Size<span style="font-weight:bold"> ${area} </span><br /> ${members}`);
+				town.weight = 1.5;
+				town.opacity = 1;
+			});
+
+			// Paint alliances.
+			if (!drawAlliances) {
+				filter.write(encoder.encode(JSON.stringify(data)));
+				filter.close();
+			}
+			else {
+				// Fetch alliances.
+				fetchAlliances(details.url.includes('nova') ? novaURL : auroraURL).then(alliances => {
+
+					Object.values(data.sets[jsonPath].areas).forEach(town => {
+
+						let townTitle = town.desc.split('<br />')[0],
+							meganationList = '';
+						townTitle = townTitle.replaceAll(/[()]/g, '').split(' ');
+						const nation = townTitle[2].replace('</span>', '');
+						if (mapMode == 'normal') town.color = town.fillcolor = '#000000';
+
+						// Paint alliances.
+						alliances.forEach(alliance => {
+							const allianceType = alliance.type ?? 'mega',
+								allianceName = alliance.fullName ?? alliance.allianceName;
+							if (allianceType != mapMode || !alliance.nations.includes(nation)) return;
+							if (alliance.colours) {
+								allianceFillColor = alliance.colours.fill;
+								allianceStrokeColor = alliance.colours.outline;
+							}
+							else { allianceFillColor = allianceStrokeColor = '#000000'; }
+							meganationList.length < 1 ? meganationList += allianceName : meganationList += ', ' + allianceName;
+							town.color = allianceStrokeColor;
+							town.fillcolor = allianceFillColor;
+						});
+
+						if (meganationList.length > 0) town.desc = town.desc.replace(')</span><br />', `)</span><br /> <span style="font-size:80%">Part of</span> <span style="font-size:90%"><b>${meganationList}</b></span><br />`);
+					});
+
+					// Sending the modified response.
+					filter.write(encoder.encode(JSON.stringify(data)));
+					filter.close();
+				}).catch((error) => {
+					console.error(`Could not fetch alliances, sending normal map: ${error}`);
+					filter.write(encoder.encode(JSON.stringify(data)));
+					filter.close();
+				});
+			}
+		}
+	};
 }
 
-// Managing the players' status response.
+// Manage the player response.
 function onPlayerUpdate(details) {
 
 	const filter = browser.webRequest.filterResponseData(details.requestId);
 	const arrayBuffer = [];
-	filter.ondata = event => arrayBuffer.push(decoder.decode(event.data, {stream: true}));
+	filter.ondata = event => arrayBuffer.push(decoder.decode(event.data, { stream: true }));
 
 	filter.onstop = () => {
+		if (date.length == 8) {
+			filter.close();
+			return;
+		}
 		arrayBuffer.push(decoder.decode());
 		const string = arrayBuffer.join('');
 		const data = JSON.parse(string);
 		if (!data.currentcount) return;
-		// If response's size > 64kB, it's a map update.
+		// If response is bigger than usual then it is map update.
 		string.length < 65536 ? filter.write(encoder.encode(JSON.stringify(data))) : onMapUpdate(details);
 		filter.close();
-	}
+	};
 }
--- firefox/content.js
+++ firefox/content.js
@@ -1,39 +1,57 @@
 const interval = setInterval(() => {
-	if (document.getElementsByClassName('coord-control coord-control-noy leaflet-control').length > 0) {
+	if (document.getElementsByClassName('coord-control coord-control-noy leaflet-control')) {
 		// Page has been loaded; remove elements.
 		document.getElementsByClassName('compass compass_S compass_flat')[0].remove();
 		document.getElementsByClassName('largeclock timeofday')[0].remove();
-		document.getElementsByClassName('leaflet-bottom leaflet-right')[0].remove();
 
-		// Create map mode button.
-		const div = document.createElement('div');
-		div.className = 'coord-control leaflet-control';
-		document.getElementsByClassName('leaflet-top leaflet-left')[0].appendChild(div);
+		// Create menu.
+		const menu = document.createElement('div');
+		menu.className = 'extension coord-control leaflet-control';
+		document.getElementsByClassName('leaflet-top leaflet-left')[0].appendChild(menu);
+		menu.style.display = 'grid';
+
+		// Create the button.
 		const button = document.createElement('button');
-		button.className = 'coord-control-button';
+		const buttonDiv = document.createElement('div');
 		button.innerHTML = 'Switch map mode';
-		div.appendChild(button);
+		buttonDiv.innerHTML = '<abbr title="Switch between alliances and meganations. Restart page if dynmap did not load correctly, and check if database is down.">(?)</abbr> ';
+		buttonDiv.appendChild(button);
+		menu.appendChild(buttonDiv);
 
-		// Detect the button click.
+		// Create the date input.
+		const date = document.createElement('input');
+		const archiveDiv = document.createElement('div');
+		archiveDiv.innerHTML = '<abbr title="View old claims & stats, does not include terrain. Switch map mode to leave the archive.">(?)</abbr> ';
+		date.min = window.location.href.includes('nova') ? '2018-12-18' : '2022-05-01';
+		date.type = 'date';
+		archiveDiv.appendChild(date);
+		menu.appendChild(archiveDiv);
+
+		// Implement listeners.
 		button.addEventListener('click', () => {
-			browser.runtime.sendMessage({message: 'Button clicked'});
+			browser.runtime.sendMessage({ message: 'Button clicked' });
+			document.location.reload();
+		});
+		date.addEventListener('change', () => {
+			browser.runtime.sendMessage({ date: date.valueAsDate });
 			document.location.reload();
 		});
 
-		// Check for available update.
-		fetch('https://raw.githubusercontent.com/3meraldK/earthmc-dynmap/main/firefox/manifest.json').then(response => response.json()).then(manifest => {
-			const localVersion = chrome.runtime.getManifest().version;
-			const latestVersion = manifest.version;
-			if (localVersion != latestVersion) {
-				// Create update available div.
-				const div = document.createElement('div');
-				div.className = 'coord-control leaflet-control';
-				document.getElementsByClassName('leaflet-top leaflet-left')[0].appendChild(div);
-				const label = document.createElement('label');
-				label.innerHTML = '<a href=\"https://github.com/3meraldK/earthmc-dynmap/releases/latest\">Extension update available</a><br>(from ' + localVersion + ' to ' + latestVersion + ')';
-				div.appendChild(label);
-			}
-		}).catch(() => {});
+		// Check for updates.
+		fetch('https://raw.githubusercontent.com/3meraldK/earthmc-dynmap/main/firefox/manifest.json')
+			.then(response => response.json())
+			.then(manifest => {
+				const localVersion = chrome.runtime.getManifest().version;
+				const latestVersion = manifest.version;
+				if (localVersion == latestVersion) return;
+
+				const updateDiv = document.createElement('div');
+				updateDiv.className = 'extension coord-control leaflet-control';
+				document.getElementsByClassName('leaflet-top leaflet-left')[0].appendChild(updateDiv);
+				const updateLabel = document.createElement('label');
+				updateLabel.innerHTML = `<a href="https://github.com/3meraldK/earthmc-dynmap/releases/latest">Extension update available</a><br>(from ${localVersion} to ${latestVersion})`;
+				updateDiv.appendChild(updateLabel);
+			}).catch((error) => { console.log(`Could not fetch latest version: ${error}`); });
 
 		clearInterval(interval);
 	}
