dcb1f77425c27da801d81f38622e2589574c0e0e
3meraldK <ooyaxcontact@gmail.com>
2026-08-08 15:51:29 +0200

    part 2

--- src/extension/main.js-appender.js
+++
@@ -1,6 +0,0 @@
-function injectMainScript() {
-	const mainScript = document.createElement('script')
-	mainScript.src = chrome.runtime.getURL('main.js')
-	mainScript.onload = function () { this.remove() };
-	(document.head || document.documentElement).appendChild(mainScript)
-}
